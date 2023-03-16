e54e55674eaf3efd7ac0f48d3d235c12acd88611
Linh <33163654+linhtng@users.noreply.github.com>
2023-03-16 19:07:21 +0200

    Add files via upload

--- includes/pipex.h
+++ includes/pipex.h
@@ -1,1 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuynguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 17:43:30 by thuynguy          #+#    #+#             */
+/*   Updated: 2023/03/08 17:45:22 by thuynguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "./ft_printf/ft_printf.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+void	free_arr(char **arr);
+void err_exit(char **mem1, char **mem2, char *message);
+char	*valid_path(char **cmd_arr, char **paths_arr);
+
+#endif
